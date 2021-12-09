ea9d6d26f5b1eadd45c62fbf3953b92e8c76216c
Piping Ferdinand <f.crayop@gmail.com>
2021-12-09 18:41:01 +0800

    test

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,46 @@
+class ProjectsController < ApplicationController
+  before_action :project_initialize, only: [ :destroy, :update]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+
+  end
+
+  def create
+    @project = Project.create(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to @projects
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @projects
+    else
+      redirect_to @project
+    end
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:description, :title)
+    end
+
+    def project_initialize
+      @project = Project.find(params[:id])
+    end
+end
